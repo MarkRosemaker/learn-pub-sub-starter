594991affb1fe401819f6cc5c74138e11c30a956
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2025-10-01 21:16:59 +0200

    Connect

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const connectionString = "amqp://guest:guest@localhost:5672/"
 
 func main() {
+	if err := do(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func do() error {
 	fmt.Println("Starting Peril server...")
+
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		return fmt.Errorf("dialing: %w", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected!")
+
+	// Wait for a signal
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+	<-sigs
+
+	fmt.Println("\nShutting down and closing the connection...")
+
+	return nil
 }
